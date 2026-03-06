8bce9dc0d63ef3e4293682b96910d71aad6c471d
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2026-03-06 18:16:01 +0100

    Inicial

--- src/gui/BotoIcona.java
+++ src/gui/BotoIcona.java
@@ -16,27 +16,27 @@ public class BotoIcona extends Boto {
     // Dibuixa el botó
     public void display(PApplet p5){
         p5.pushStyle();
-        if(!activat){
-            p5.fill(colorFarcimentDesactivat);  // Color desabilitat
+        if(!this.activat){
+            p5.fill(this.colorFarcimentDesactivat);  // Color desabilitat
         }
-        else if(mouseDins(p5)){
-            p5.fill(colorFarcimentActiu);      // Color quan ratolí a sobre
+        else if(super.mouseDins(p5)){
+            p5.fill(this.colorFarcimentActiu);      // Color quan ratolí a sobre
         }
         else{
-            p5.fill(colorFarciment);          // Color actiu però ratolí fora
+            p5.fill(this.colorFarciment);          // Color actiu però ratolí fora
         }
-        p5.stroke(colorContorn);
+        p5.stroke(this.colorContorn);
         p5.strokeWeight(GRUIX_BOTO);        //Color i gruixa del contorn
         p5.rect(this.x, this.y, this.w, this.h, 10);    // Rectangle del botó
 
         // Text (color, alineació i mida)
         p5.fill(0); p5.textAlign(p5.CENTER, p5.CENTER);
-        p5.textFont(fonts.getFontSecundaria());
+        p5.textFont(this.fonts.getFontSecundaria());
         p5.textSize(TEXT_BOTO);
-        p5.text(textBoto, this.x + this.w/2, this.y + this.h/2 - 8);
+        p5.text(this.textBoto, this.x + this.w/2, this.y + this.h/2 - 8);
 
         p5.fill(255, 100, 100);
-        p5.textFont(fonts.getFontEmoji());
+        p5.textFont(this.fonts.getFontEmoji());
         p5.textSize(TEXT_ICONA_BOTO); p5.textAlign(p5.LEFT, p5.CENTER);
         p5.text(new String(Character.toChars(codiIcona)), this.x + 10, this.y + this.h/2 - 2);
         p5.popStyle();
